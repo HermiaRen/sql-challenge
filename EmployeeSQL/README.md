bf9242335608fb00c1152a2da4670e280a907059
HermiaRen <issiliasette@IssiliadeMacBook-Air.local>
2023-08-24 14:27:54 +0930

    completed

---
+++ EmployeeSQL/README.md
@@ -0,0 +1,17 @@
+## Repository Structure
+
+- The `output` folder contains all the CSV files for the data analysis part.
+  - `list1.csv`: Lists the employee number, last name, first name, sex, and salary of each employee.
+  - `list2.csv`: Lists the first name, last name, and hire date for employees hired in 1986.
+  - `list3.csv`: Lists the manager of each department along with their department number, department name, employee number, last name, and first name.
+  - `list4.csv`: Lists the department number for each employee along with their employee number, last name, first name, and department name.
+  - `list5.csv`: Lists the first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+  - `list6.csv`: Lists each employee in the Sales department, including their employee number, last name, and first name.
+  - `list7.csv`: Lists each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+  - `list8.csv`: Lists the frequency counts, in descending order, of all employee last names (i.e., how many employees share each last name).
+
+- The `data_preview.ipynb` notebook demonstrates the process of previewing the CSV files in the `data` folder.
+- `schemas.sql` contains the code for the data engineering part.
+- `query.sql` contains the code for the data analysis part.
+- The `output` folder also contains `QuickDBD-sql-challenge.png`, which represents the Entity-Relationship Diagram (ERD).
+
---
+++ EmployeeSQL/schemas.sql
@@ -0,0 +1,63 @@
+﻿-- departments
+CREATE TABLE "departments" (
+    "dept_no" varchar NOT NULL PRIMARY KEY,
+    "dept_name" varchar NOT NULL
+);
+
+-- titles
+CREATE TABLE "titles" (
+    "title_id" varchar NOT NULL PRIMARY KEY,
+    "title" varchar NOT NULL
+);
+
+-- employees
+CREATE TABLE "employees" (
+    "emp_no" varchar NOT NULL PRIMARY KEY,
+    "emp_title_id" varchar NOT NULL,
+    "birth_date" date NOT NULL,
+    "first_name" varchar NOT NULL,
+    "last_name" varchar NOT NULL,
+    "sex" varchar NOT NULL,
+    "hire_date" date NOT NULL
+);
+
+-- salaries
+CREATE TABLE "salaries" (
+    "emp_no" varchar NOT NULL PRIMARY KEY,
+    "salary" decimal NOT NULL
+);
+
+-- dept_manager
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar NOT NULL,
+    "emp_no" varchar NOT NULL
+);
+
+-- dempt_emp
+CREATE TABLE "dempt_emp" (
+    "emp_no" varchar NOT NULL,
+    "dept_no" varchar NOT NULL
+);
+
+-- Foreign key constraints
+
+-- dempt_emp
+ALTER TABLE "dempt_emp" ADD CONSTRAINT "fk_dempt_emp_emp_no" FOREIGN KEY ("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dempt_emp" ADD CONSTRAINT "fk_dempt_emp_dept_no" FOREIGN KEY ("dept_no")
+REFERENCES "departments" ("dept_no");
+
+-- dept_manager
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY ("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY ("emp_no")
+REFERENCES "employees" ("emp_no");
+
+-- employees
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY ("emp_no")
+REFERENCES "salaries" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY ("emp_title_id")
+REFERENCES "titles" ("title_id");
